4de5ec90eae970f100db2447c7435303e9d904b9
lbcat <lightbulbcat@icloud.com>
2017-10-08 19:07:54 +0900

    add LoginForm

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import { auth } from './actions'
 
-const InputForm = ({ label, ...inputProps}) => {
+const InputFormGroup = ({ label, ...inputProps}) => {
   return (
     <FormGroup>
       <Label for={inputProps.id}>{label}</Label>
@@ -11,22 +12,33 @@ const InputForm = ({ label, ...inputProps}) => {
   )
 }
 
-const LoginForm = ({ dispatch }) => {
+export class LoginForm extends React.Component {
 
-  const onSubmit = event => {
-    console.log('submit', event)
-  }
-  const onChange = event => {
-    console.log('change', event)
-  }
+  state = { user: undefined }
 
-  return (
-    <Form onSubmit={onSubmit}>
-      <InputForm label="User Name" type="text" name="email"
-        onChange={onChange} />
-      <Button color="secondary" type="submit">Submit</Button>
-    </Form>
-  )
+  render() {
+
+    const handleChange = event => {
+      const newState = { user: event.target.value }
+      this.setState(() => newState)
+    }
+
+    const handleSubmit = event => {
+      this.props.onSubmit(this.state)
+    }
+
+    return (
+      <Form>
+        <InputFormGroup label="User Name" type="text" name="email" onChange={handleChange} />
+        <Button onClick={handleSubmit} color="secondary">Submit</Button>
+      </Form>
+    )
+  }
 }
 
-export default LoginForm
+export const LoginFormWithStore = connect()(props => {
+  const onSubmit = event => props.dispatch(auth(event.user))
+  return <LoginForm onSubmit={onSubmit} />
+})
+
+export default LoginFormWithStore
